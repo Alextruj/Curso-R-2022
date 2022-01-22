51d1a7bd28fa8e44f40806b5e7572620301bd84c
Alextruj <36289196+Alextruj@users.noreply.github.com>
2022-01-21 18:10:53 -0600

    Add files via upload

---
+++ Funciones base.R
@@ -0,0 +1,22 @@
+########### FUNCIONES BASE ################
+
+### DIRECTORIO DE TRABAJO
+## CONSULTAR
+getwd()
+## ESTABLECER
+setwd("C:/Users/K17095/Documents/Curso macro R/Fundamentales")
+
+
+### AYUDA DE FUNCIONES
+help(mean)
+?mean
+
+
+### FUNCIONES MUY UTILES
+## IMPRIMIR
+print("Hola mundo")
+
+## CONCATENAR
+x <- paste("Hola", 2)
+
+
