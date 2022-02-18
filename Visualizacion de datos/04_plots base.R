6f8b53afd9d19b11a3d7429f43df095e08621361
Alextruj <36289196+Alextruj@users.noreply.github.com>
2022-02-17 19:45:05 -0600

    Add files via upload

---
+++ Visualizacion de datos/04_plots base.R
@@ -0,0 +1,42 @@
+########### PLOTS BASE ################
+
+### CARGUEMOS UN DATAFRAME DEFAULT
+data <- mtcars
+
+### PRIMERAS OBSERVACIONES
+head(data, n = 10)
+
+
+### SCATTERPLOT
+plot(x = data$wt, y = data$mpg,
+     pch = 16, col = "firebrick",
+     xlab = "wt", ylab = "mpg")
+
+
+### LINEAS
+library(readxl)
+datats <- read_excel("C:/Users/K17095/Documents/Curso macro R/Datos/IPUS_fred.xlsx")
+plot(x = datats$date, y = datats$value,
+     type = "l", col = "firebrick",
+     xlab = "Fecha", ylab = "Indice",
+     main = "Indice de Produccion Industrial (US)")
+
+
+### LEYENDA
+legend("topleft", legend = c("Argentina", "Estados Unidos", "Mexico"),
+       col = c("blue", "red", "darkgreen"), pch=1)
+
+
+### HISTOGRAMA
+hist(x = data$cyl)
+
+
+
+### BOXPLOTS
+boxplot(x = data$mpg)
+
+boxplot(data$mpg ~ data$cyl,
+        xlab = "Cilindros", ylab = "Millas por galón")
+
+
+
